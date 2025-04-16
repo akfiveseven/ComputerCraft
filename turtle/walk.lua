4577c015fcbb9da9390f267a33153eafd82df847
Ammar <ammarfiveseven@gmail.com>
2025-04-16 16:44:00 -0700

    fixed walk

--- turtle/walk.lua
+++ turtle/walk.lua
@@ -1,17 +1,15 @@
 local core = require("turtle.core")
 local args = {...}
 
-for i = 1, tonumber(args[1]) do
-    if core.is_enough_fuel(tonumber(args[1])) then
-        for i = 1, i do
-            if turtle.detect() == false then
-                print("Walk " .. i)
-                turtle.forward()
-            else
-                print("[ERROR] Walk: obstruction")
-            end
+if core.is_enough_fuel(tonumber(args[1])) then
+    for j = 1, tonumber(args[1]) do
+        if turtle.detect() == false then
+            print("Walk " .. i)
+            turtle.forward()
+        else
+            print("[ERROR] Walk: obstruction")
         end
-    else
-        print("[ERROR] Walk: fuel")
     end
+else
+    print("[ERROR] Walk: fuel")
 end
