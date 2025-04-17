fa890d8795b2f4ec8ece66462ac07b0aca891724
Ammar <ammarfiveseven@gmail.com>
2025-04-16 18:08:26 -0700

    fixed bridge

--- turtle/bridge.lua
+++ turtle/bridge.lua
@@ -2,14 +2,15 @@ local args = {...}
 
 local length = tonumber(args[1])
 
+
 if turtle.getFuelLevel() < length+1 then
     print("[ERROR] Bridge: fuel")
 end
 turtle.up()
-if t.getItemCount() >= length then
+if turtle.getItemCount() >= length then
     for i = 1, length do
         shell.run("walk 1")
-        t.placeDown()
+        turtle.placeDown()
     end
 else
     print("[ERROR] Bridge: item count")
